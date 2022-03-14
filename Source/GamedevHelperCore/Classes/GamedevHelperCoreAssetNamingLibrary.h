6fa568aeb031688d9490251eda9a2d231a06975f
Ashot Barkhudaryan <ashot7410@gmail.com>
2022-03-14 18:58:06 +0400

    feat: add asset naming library class
    - added empty skeleton

---
+++ Source/GamedevHelperCore/Classes/GamedevHelperCoreAssetNamingLibrary.h
@@ -0,0 +1,16 @@
+﻿// Copyright Ashot Barkhudaryan. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GamedevHelperCoreTypes.h"
+#include "GamedevHelperCoreAssetNamingLibrary.generated.h"
+
+UCLASS()
+class GAMEDEVHELPERCORE_API UGamedevHelperAssetNamingLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	// todo:ashe23
+};
