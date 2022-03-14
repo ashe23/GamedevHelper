d022b92a18ccb5a2a339126fb2b67d7a52f66104
Ashot Barkhudaryan <ashot7410@gmail.com>
2022-03-14 14:57:37 +0400

    feat: add gamedev helper editor module

---
+++ Source/GamedevHelperEditor/Private/GamedevHelperEditor.cpp
@@ -0,0 +1,28 @@
+﻿// Copyright Ashot Barkhudaryan. All Rights Reserved.
+
+#include "GamedevHelperEditor.h"
+
+#define LOCTEXT_NAMESPACE "FGamedevHelperEditor"
+
+class FGamedevHelperEditor : public IGamedevHelperEditor
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+void FGamedevHelperEditor::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FGamedevHelperEditor::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FGamedevHelperEditor, GamedevHelperEditor)
+DEFINE_LOG_CATEGORY(LogGamedevHelperEditor);
---
+++ Source/GamedevHelperEditor/Public/GamedevHelperEditor.h
@@ -0,0 +1,28 @@
+﻿// Copyright Ashot Barkhudaryan. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleInterface.h"
+#include "Modules/ModuleManager.h"
+
+namespace ModuleConstants
+{
+	const FName ModuleName{TEXT("GamedevHelperEditor")};
+}
+
+class IGamedevHelperEditor : public IModuleInterface
+{
+public:
+	static IGamedevHelperEditor& Get()
+	{
+		return FModuleManager::LoadModuleChecked<IGamedevHelperEditor>(ModuleConstants::ModuleName);
+	}
+
+	static bool IsAvailable()
+	{
+		return FModuleManager::Get().IsModuleLoaded(ModuleConstants::ModuleName);
+	}
+};
+
+DECLARE_LOG_CATEGORY_EXTERN(LogGamedevHelperEditor, Log, All);
