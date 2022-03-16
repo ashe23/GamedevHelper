9bb6c11d17cb8ce3ce11ca69721defbceef0e1bf
ashe23 <ashot7410@gmail.com>
2022-03-16 04:14:15 +0400

    feat: add asset naming manager library class

---
+++ Source/GamedevHelper/Classes/GamedevHelperAssetNamingManagerLibrary.h
@@ -0,0 +1,21 @@
+﻿// Copyright Ashot Barkhudaryan. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GamedevHelperAssetNamingManagerLibrary.generated.h"
+
+UCLASS()
+class UGamedevHelperAssetNamingManagerLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category="GamedevHelper|AssetNamingManager")
+	static FString Normalize(const FString& OriginalString);
+
+	UFUNCTION(BlueprintCallable, Category="GamedevHelper|AssetNamingManager")
+	static FString Tokenize(const FString& OriginalString);
+
+	
+};
---
+++ Source/GamedevHelper/Private/GamedevHelperAssetNamingManagerLibrary.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright Ashot Barkhudaryan. All Rights Reserved.
+
+#include "GamedevHelperAssetNamingManagerLibrary.h"
+
+FString UGamedevHelperAssetNamingManagerLibrary::Normalize(const FString& OriginalString)
+{
+	// todo:ashe23
+	return FString{};
+}
+
+FString UGamedevHelperAssetNamingManagerLibrary::Tokenize(const FString& OriginalString)
+{
+	// todo:ashe23
+	return FString{};
+}
