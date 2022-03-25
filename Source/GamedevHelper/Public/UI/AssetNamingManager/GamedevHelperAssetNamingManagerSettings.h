d9e56e5540afbce406f666bbff8ffb8a9f64c299
ashe23 <ashot7410@gmail.com>
2022-03-26 01:11:34 +0400

    doc: asset naming manager settings tooltips
    Ref: #20

--- Source/GamedevHelper/Public/UI/AssetNamingManager/GamedevHelperAssetNamingManagerSettings.h
+++ Source/GamedevHelper/Public/UI/AssetNamingManager/GamedevHelperAssetNamingManagerSettings.h
@@ -21,18 +21,18 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AssetNamingManagerSettings", meta = (ContentDir))
 	FDirectoryPath ScanPath;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AssetNamingManagerSettings")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AssetNamingManagerSettings", meta = (ToolTip = "Scan folder recursively"))
 	bool bScanRecursive;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AssetNamingManagerSettings")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AssetNamingManagerSettings", meta = (ToolTip = "If enabled will show assets whose classes are missing in naming convention list"))
 	bool bShowMissingTypes;
 	
 	FAssetNamingManagerSettingsChangeDelegate OnSettingsChangeDelegate;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AssetNamingManagerSettings")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AssetNamingManagerSettings", meta = (ToolTip = "List of prefixes that will be removed from asset name, when renaming"))
 	TArray<FString> OldPrefixes;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AssetNamingManagerSettings")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AssetNamingManagerSettings", meta = (ToolTip = "List of suffixes that will be removed from asset name, when renaming"))
 	TArray<FString> OldSuffixes;
 
 private:
