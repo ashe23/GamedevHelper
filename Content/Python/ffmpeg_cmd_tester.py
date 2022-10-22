8e4930bfc1442914ed41fce93976e7f301327873
Ashot Barkhudaryan <ashot7410@gmail.com>
2022-10-22 17:35:10 +0400

    refactor: rendering manager
    - removed old python scripts

--- Content/Python/ffmpeg_cmd_tester.py
+++
@@ -1,9 +0,0 @@
-﻿import subprocess
-import unreal
-
-# we will receive command that user picks from unreal editor and check if its valid command, and report back to editor, so user can check realtime if command valid or not
-cmd = "ffmpeg.exe -i"
-output = subprocess.call(cmd, shell=True)
-unreal.log(output)
-# if subprocess.call(cmd, shell=True) != 0:
-#     unreal.log_error("Cmd failed {}".format(cmd))
