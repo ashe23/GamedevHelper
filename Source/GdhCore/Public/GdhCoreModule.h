9277067912134be48c1b9e03d077453f71955ec6
Ashot Barkhudaryan <ashot.barkhudaryan23@gmail.com>
2025-01-29 14:06:07 +0400

    repo: added formatting files (clang-format, clang-tidy and editorconfig)

--- Source/GdhCore/Public/GdhCoreModule.h
+++ Source/GdhCore/Public/GdhCoreModule.h
@@ -9,7 +9,8 @@ DECLARE_LOG_CATEGORY_EXTERN(LogGdhCore, Log, All);
 
 class FGdhCoreModule final : public IModuleInterface
 {
-public:
+  public:
+
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
 };
